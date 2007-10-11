1785a174d5b6b42dd53a460bd0e203ebfe53b777
robert.allen.krause <robert.allen.krause@48e96aa7-5239-0410-8836-99421de08570>
2007-10-11 07:39:17 +0000

    Consolidated Zodiac Client into a single header file. Says which static libraries to link to in the file. It is confirmed to work.

--- trunk/server/ZodiacServer.java
+++ trunk/server/ZodiacServer.java
@@ -1,39 +1,52 @@
-import java.io.*;
-import java.util.LinkedList;
-import java.net.*;
 
-//sample code for server.
-//very bad code, but it looks like it works. I don't have the java sdk however to test that. Feel free to implement in whatever as long as it uses a socket
-//which should be specified on the standards page at some point. This will put all of the keys sent into a text file.
-
-public class ZodiacServer implements Runnable {
-	LinkedList queue;
-	Thread th;
-	public static void main(String[] args) {
-		ZodiacServer zs = new ZodiacServer();
-		Thread t = new Thread(zs);
-		zs.th = t;
-		t.start();
-		SocketServer ss = new SocketServer(10000);
-		while(true) {
-			zs.queue.append(ss.accept());
-		}
-	}
-	public ZodiacServer() {
-		queue = new LinkedList();
-	}
-	public void run() {
-		while(true) {
-			th.sleep(1000);
-			if(queue.size()>0) {
-				Object o = queue.remove();
-				Socket s = (Socket)o;
-				System.out.println(new BufferedReader(s.getOutputStream()).read());
-/*				FileWriter f = new FileWriter("textfile.txt",true);
-				f.write(new BufferedReader(s.getOutputStream()).read());
-				f.flush();
-				f.close();*/
-			}
-		}
-	}
+import java.io.*; 
+import java.util.*; 
+import java.net.*; 
+ 
+//sample code for server. 
+//very bad code, but it looks like it works. I don't have the java sdk however to test that. Feel free to implement in whatever as long as it uses a socket 
+//which should be specified on the standards page at some point. This will put all of the keys sent into a text file. 
+ 
+public class jServer implements Runnable { 
+     LinkedList queue,q2,q3; 
+     Thread th; 
+     public static void main(String[] args) throws IOException { 
+          jServer zs = new jServer(); 
+          Thread t = new Thread(zs); 
+          zs.th = t; 
+          t.start(); 
+          ServerSocket ss = new ServerSocket(10000); 
+          while(true) { 
+               zs.q2.add(ss.accept()); 
+               zs.q3.add(new BufferedReader(new InputStreamReader(((Socket)zs.q2.getLast()).getInputStream()))); 
+          } 
+     } 
+     public jServer() { 
+          queue = new LinkedList(); 
+          q2 = new LinkedList(); 
+          q3 = new LinkedList(); 
+     } 
+     public void run() { 
+          while(true) { 
+               try { 
+                    th.sleep(1000); 
+                    ListIterator i = q2.listIterator(); 
+                    ListIterator i2 = q3.listIterator(); 
+                    while(i.hasNext()&&i2.hasNext()) { 
+                         BufferedReader r = (BufferedReader)i2.next(); 
+                         Object o = r.ready()?queue.add(r):"LOL!"; 
+                    } 
+                    while(queue.size()>0) { 
+                         Object o = queue.remove(); 
+                         BufferedReader s = (BufferedReader)o; 
+                         System.out.println(s.readLine()); 
+ 
+          /*                    FileWriter f = new FileWriter("textfile.txt",true); 
+                         f.write(new BufferedReader(s.getOutputStream()).read()); 
+                         f.flush(); 
+                         f.close();*/ 
+                    } 
+               } catch (Exception e) {e.printStackTrace();} 
+          } 
+     } 
 }
