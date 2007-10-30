d1be8b5fd11d73cc2db7904e9f87cb5acc28039d
robert.allen.krause <robert.allen.krause@48e96aa7-5239-0410-8836-99421de08570>
2007-10-30 06:47:23 +0000

    client functionality

--- SuperClient/SuperClient.cpp
+++ SuperClient/SuperClient.cpp
@@ -1,15 +1,52 @@
 #include "Client.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-int main(){
-	char allo[10][63];
-	if(!getKeysFromServer((char **&)allo)){
-		for(int i = 0; i < 10; i++)
-			printf("%s\n", allo[i]);
+using namespace std;
+
+int main()
+{
+	char key[63];
+	ifstream fin;
+	fin.open("goodkey.txt", ios::in);
+	fin >> key;
+
+	if(sendToServer("zodiacdecoder.dyndns.org","10002",key))
+	{
+		fin.close();
+		return 1;
+	}
+	fin.close();
+	return 0;
+}
+
+/*int main(){
+	
+	char allo[63];
+	for(int j = 0; j<63; j++){
+			allo[j] = 'a';
+			//printf("%c", allo[j]);
+	
+	}
+	fstream fout;
+	fout.open("superclientkeys.txt", ios::out);
+	if(!getKeysFromServer(allo)){
+		for(int i = 0; i < 63; i++){
+			fout << allo[i];
+		}
 	}else
 		printf("You're a tard");
-	if(!vote("64.233.167.99", "000000000000000000000000000000000000000000000000000000000000000", FALSE))
-		printf("Hallo!");
-	else
-		printf("Wow, you really suck!");
+	fout.close();
+	
+
   return 0;
 }
+*/
+/*
+int main(){
+	char allo[63];
+	fin 
+	sendToServer("zodiacdecoder.dyndns.org", "10000", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabb");
+	return 0;
+}*/
