f29203eaf4ea9d3b34294cb18bc2527fe76dd4d7
ohappyguy <ohappyguy@48e96aa7-5239-0410-8836-99421de08570>
2007-11-05 03:24:13 +0000

    Added items remotely
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\riTole2.h
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\riToleleTori.h
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\topTobot.h
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\topTobotbotTotop2.h
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\340.txt
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\launch.c
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\botTotop.h
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\botTotoptopTobot.h
    C:\Users\Kevin\FALL 2007\CSCE 3210 Symbolic Processing\csce3210\New\launch\leToririTole.h

---
+++ trunk/Existing Tool/Version2/botTotoptopTobot.h
@@ -0,0 +1,81 @@
+//Performs the columnar transposition encription or decryption.
+//Read the file 340.txt from bottom to top and top to bottom. 
+
+#include <stdio.h>
+
+void botTotoptopTobot() 
+{
+	char buffer[20][17] = {{0},{0}};  /* initialized to zeros */
+	char tmp[17][20] = {{0}, {0}};
+	int i, j;
+	char filename[BUFSIZ +1];
+	FILE *ifp = fopen("340.txt", "r");
+	FILE *ofp;
+	char ch;	
+	int x, n;
+	
+	if (ifp == NULL) {
+        printf("Couldn't open the file!\n");
+//        return 0;
+    	}
+
+    	for (i = 0; i < 20; i++) {
+		for (j = 0; j < 18; j++) {
+			ch = fgetc(ifp);
+			if (ch != '\n' && j <= 17) {
+	       			buffer[i][j] = ch;
+	       		}
+		}
+	}
+	printf("\n\t    Plain Text\t\tRead Bottom to Top\n");
+	printf("\t\t\t\t and Top to Bottom\n\n");
+
+	ofp = fopen("botTotoptopTobot.txt", "w");
+	for(j=0; j<17;j++){
+		x=j;
+		x=x%2;	
+		if (x==1) { 
+			for (i=0; i < 20; i++) {
+				tmp[j][i]=buffer[i][j];
+			}	
+		}//read top to bottom
+	
+		if (x==0) { 
+			for(i=19, n=0; i >= 0 && n<20; n++, i--) {
+				tmp[j][n]=buffer[i][j];
+			}	
+		}
+	}
+
+	for(i=0; i<20; i++) {
+		printf("  \t");
+		fputc('\t', ofp);
+			for(j=0; j<17; j++) {
+				printf("%c", buffer[i][j]);
+			}
+			printf("\t");
+			for (n=0; n<20; n++) {
+				if(i<17) {
+					printf("%c", tmp[i][n]);
+					fputc(tmp[i][n], ofp);
+				}
+				if(i>17) {
+					printf(" ");
+					fputc(' ', ofp);
+				}
+			}
+			printf("\n");
+			fputc('\n',ofp);
+	}
+			
+	if(ofp == NULL) {
+	fprintf(stderr, "Can't open output file %s!\n", "toptobotbottotop.txr");	
+	exit(1);
+	}	
+		printf("\n\t\tSuccessfully converted!\n\n");
+	fclose(ifp);
+	fclose(ofp);
+	
+}
+
+
