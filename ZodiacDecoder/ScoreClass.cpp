f263d1a2bcaba47b62084e12d8e7e32fef223b07
robert.allen.krause <robert.allen.krause@48e96aa7-5239-0410-8836-99421de08570>
2007-11-30 00:17:29 +0000

    human reader has no issues with server. umm. depends on your network connection I guess. also, added the latest client version. yeah, it's nice I guess. oh yeah, wrote up a schematic for cold fusion reactor.

--- ZodiacDecoder/ScoreClass.cpp
+++ ZodiacDecoder/ScoreClass.cpp
@@ -289,6 +289,8 @@ float ScoreClass::ScoreSolution(string solution)
 		letterScore = letterScore - fabs(letterFreqs[l] - cipherFreqs[l]);
 	}
 
+	//cout<<"W: "<<wordScore<<" L: "<<letterScore<<" N: "<<nGraphScore<<endl;
+	
 	//calculate total score
 	totalScore = (nGraphScore * 0.33333) + (wordScore * 0.33333) + (letterScore * 0.33333);
 
